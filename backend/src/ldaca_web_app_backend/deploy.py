14677d7c61d3152335b7e53468acec23d888be87
Senhui (Alex) Guo <general@sguo.org>
2025-09-23 15:58:28 +1000

    fix: Update Python version requirement to >=3.12, add jupyter-server-proxy dependency, and enhance frontend deployment logic

--- backend/src/ldaca_web_app_backend/deploy.py
+++ backend/src/ldaca_web_app_backend/deploy.py
@@ -50,21 +50,32 @@ def start_backend(port=8001):
     return _server_task
 
 
-def start_frontend(port=3000, platform=None):
+def start_frontend(port=3000, platform=None, use_latest=False):
     url = f"http://localhost:{port}"
-    FRONTEND_RELEASE_URL = "https://github.com/Australian-Text-Analytics-Platform/ldaca_web_app/releases/download/frontend-latest/frontend-build.tar.gz"
-
-    response = requests.get(FRONTEND_RELEASE_URL)
-    response.raise_for_status()  # Raise error for bad status codes
-    with tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False) as temp_file:
-        temp_file.write(response.content)
-        temp_file_path = temp_file.name
-
     DIST_DIR = tempfile.mkdtemp(prefix="ldaca_frontend_build_")
-    print(f"Extracting frontend to {DIST_DIR}")
     os.makedirs(DIST_DIR, exist_ok=True)
-    with tarfile.open(temp_file_path, "r:gz") as tar:
-        tar.extractall(path=DIST_DIR)
+
+    if use_latest:
+        FRONTEND_RELEASE_URL = "https://github.com/Australian-Text-Analytics-Platform/ldaca_web_app/releases/download/frontend-latest/frontend-build.tar.gz"
+
+        response = requests.get(FRONTEND_RELEASE_URL)
+        response.raise_for_status()  # Raise error for bad status codes
+        with tempfile.NamedTemporaryFile(suffix=".tar.gz", delete=False) as temp_file:
+            temp_file.write(response.content)
+            temp_file_path = temp_file.name
+
+        print(f"Extracting frontend to {DIST_DIR}")
+        with tarfile.open(temp_file_path, "r:gz") as tar:
+            tar.extractall(path=DIST_DIR)
+    else:
+        FRONTEND_ROOT = PROJECT_ROOT / ".." / "frontend"
+
+        subprocess.run(
+            f"cd {FRONTEND_ROOT} && BUILD_PATH={DIST_DIR} npm install && npm run build",
+            check=True,
+            shell=True,
+        )
+
     NGINX_CONF_TEMPLATE = PROJECT_ROOT / "configs" / "nginx.conf.template"
     NGINX_OUTPUT_CONF = tempfile.NamedTemporaryFile(suffix=".conf", delete=False).name
 
@@ -74,7 +85,7 @@ def start_frontend(port=3000, platform=None):
         shell=True,
     )
     print(f"Using nginx config file: {NGINX_OUTPUT_CONF}")
-    subprocess.run(f"nginx -c {NGINX_OUTPUT_CONF}", check=True, shell=True)
+    subprocess.run(f"nginx -s reload -c {NGINX_OUTPUT_CONF}", check=False, shell=True)
 
     if ON_COLAB:
         output.serve_kernel_port_as_window(port)
@@ -82,16 +93,9 @@ def start_frontend(port=3000, platform=None):
         import IPython.display as display
         from IPython.display import Javascript, Markdown
 
-        base = (
-            os.environ.get("JUPYTERHUB_SERVICE_PREFIX")
-            or os.environ.get("JUPYTER_SERVER_URL")
-            or os.environ.get("JUPYTER_SERVER_ROOT")
-            or ""
-        )
+        base = os.environ["JUPYTERHUB_SERVICE_PREFIX"]
         if base and not base.endswith("/"):
             base += "/"
 
         url = f"{base}proxy/{3000}/"
-
-        display(Javascript(f"window.open('{url}', '_blank');"))
-        display(Markdown(f"If popup was blocked, click: [Open Frontend]({url})"))
+        display(Markdown(f"If popup was blocked, click: [Open web app]({url})"))
